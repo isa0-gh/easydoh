b4bc5ea2cabb9c6142569b2c5d0e84bcf2a2c32d
isa0-gh <0isa@proton.me>
2025-12-11 18:09:56 +0300

    basic resolver added

---
+++ resolver/resolver.go
@@ -0,0 +1,32 @@
+package resolver
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"time"
+)
+
+func CloudflareDoH(dnsmessage []byte) ([]byte, error) {
+	var body []byte
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest("POST", "https://1.1.1.1/dns-query", bytes.NewReader(dnsmessage))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/dns-message")
+	req.Header.Set("Content-Type", "application/dns-message")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err = io.ReadAll(resp.Body)
+
+	return body, err
+
+}
